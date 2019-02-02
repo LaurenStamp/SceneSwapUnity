56b5ec63aba13971dfc59b04be7c718c172dec98
LaurenStamp <47026726+LaurenStamp@users.noreply.github.com>
2019-02-02 12:06:03 +1100

    Scene Swap Script

---
+++ SceneSwap.cs
@@ -0,0 +1,15 @@
+﻿// Used in Impaired by Lauren Stamp
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Changes scene - used on multiple buttons in game
+public class SceneSwap : MonoBehaviour
+{
+	public void NextScene(string name)
+	{
+		SceneManager.LoadScene (name);
+	}
+
+}
